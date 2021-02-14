ea7a89b1a54880b80bedaf7bdb5a576b32ca7389
Oliver Dipple <diplomat@fermain.io>
2021-02-14 18:06:12 +0200

    Removed margin top

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 		</div><!-- .container -->
 	</div><!-- #content -->
     <?php get_template_part( 'footer-widget' ); ?>
-	<footer id="colophon" class="site-footer bg-dark text-white py-5 mt-4" role="contentinfo">
+	<footer id="colophon" class="site-footer bg-dark text-white py-5" role="contentinfo">
         <div class="container">
             <div class="row">
                 <div class="col-6 col-md-3">
@@ -73,7 +73,6 @@
     </footer><!-- #colophon -->
 <?php endif; ?>
 </div><!-- #page -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 <?php wp_footer(); ?>
 </body>
 </html>
